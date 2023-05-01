52ca9b336aebbe31402736845caa2cc5e689b207
SamyWarnants <114075972+SamyWarnants@users.noreply.github.com>
2023-05-01 21:46:18 +0200

    Finale versie
    Alles was gelukt buiten het clearen van de maximum value voor de 2de reeks cijfers

---
+++ udpclient4_project.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <string.h>
+
+#ifdef _WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#else
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#endif
+
+int main(int argc, char *argv[]) {
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2,0), &wsaData);
+
+    struct addrinfo internet_address_setup;
+    struct addrinfo *internet_address = NULL;
+    memset(&internet_address_setup, 0, sizeof internet_address_setup);
+    internet_address_setup.ai_family = AF_UNSPEC;
+    internet_address_setup.ai_socktype = SOCK_DGRAM;
+    getaddrinfo("::1", "56013", &internet_address_setup, &internet_address);
+
+    int internet_socket;
+    internet_socket = socket(internet_address->ai_family, internet_address->ai_socktype, internet_address->ai_protocol);
+
+    char go_message[] = "GO";
+    sendto(internet_socket, go_message, strlen(go_message), 0, internet_address->ai_addr, internet_address->ai_addrlen);
+    printf("%s\n", go_message);
+
+    char buffer[1000];
+    socklen_t internet_address_length = internet_address->ai_addrlen;
+    int max_value = -2147483647; // set initial value to very low value
+    int timeout_value = 1000; // set timeout to 1 second
+    setsockopt(internet_socket, SOL_SOCKET, SO_RCVTIMEO, (const char*)&timeout_value, sizeof timeout_value); // set receive timeout
+
+    while (1) {
+        int number_of_bytes_received = recvfrom(internet_socket, buffer, (sizeof buffer) - 1, 0, internet_address->ai_addr, &internet_address_length);
+        buffer[number_of_bytes_received] = '\0';
+
+        if (strcmp(buffer, "OK") == 0) {
+            printf("Received %s, shutting down\n", buffer);
+            break; // exit loop if OK is received
+        }
+
+        // process the received set of numbers
+        int received_value;
+        int local_max_value = -2147483647;
+        char *token = strtok(buffer, ",");
+        while (token != NULL) {
+            received_value = atoi(token);
+            printf("Last received value: %d\n", received_value);
+            if (received_value > local_max_value) {
+                local_max_value = received_value;
+            }
+            token = strtok(NULL, ",");
+        }
+
+        if (local_max_value > max_value) {
+            max_value = local_max_value;
+            char max_value_string[10];
+            sprintf(max_value_string, "%d", max_value);
+            sendto(internet_socket, max_value_string, strlen(max_value_string), 0, internet_address->ai_addr, internet_address->ai_addrlen);
+            printf("Max value updated: %d\n", max_value);
+        }
+    }
+
+    freeaddrinfo(internet_address);
+    closesocket(internet_socket);
+
+    WSACleanup();
+    return 0;
+}
